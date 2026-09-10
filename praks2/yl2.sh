b210f7e04619015a2d3f7c70a3954fd7abe3fb0f
Aapo Põder* <aapo.poder@voco.ee>
2026-09-10 14:34:27 +0300

    Kuupäeva ja kalendri skript yl2.sh

---
+++ praks2/yl2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Skripti eesmärk: Väljastada kasutajale praegune kuupäev, kellaaeg ja jooksva kuu kalender
+
+# Kuupäeva ja kellaaja väljastamine
+echo "Praegune kuupäev ja kellaaeg:"
+date
+
+echo "" # Tühirida loetavuse tagamiseks
+
+# Jooksva kuu kalendri väljastamine
+echo "Jooksva kuu kalender:"
+cal
